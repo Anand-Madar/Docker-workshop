75a5f6616f233eaef41299c2010f6eba1f17a362
Anand-Madar <anand.madar56@gmail.com>
2026-09-24 14:23:59 +0000

    Recover Terraform files and migrate to GCP VM

---
+++ pipeline/variables.tf
@@ -0,0 +1,24 @@
+variable "project" {
+  description = "Project ID"
+  default     = "test-terraform-507819"
+}
+
+variable "region" {
+  description = "Project Region"
+  default     = "us-central1"
+}
+
+variable "location" {
+  description = "Project Location"
+  default     = "US"
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset Name"
+  default     = "demo_dataset"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  default     = "test-terraform-507819-terra-bucket"
+}
